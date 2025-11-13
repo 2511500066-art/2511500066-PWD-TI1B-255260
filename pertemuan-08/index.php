de61c73f88f04676055418e29b47bab365f6f323
M Kaesa Dude Laudianis Koto <2511500066@mahasiswa.atmaluhur.ac.id>
2025-11-13 11:13:14 +0700

    copy index pertemuan 06 ke pertemuan 08

---
+++ pertemuan-08/index.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+
+$sesnama = "";
+if (isset($_SESSION["sesnama"])):
+  $sesnama = $_SESSION["sesnama"];
+endif;
+
+$sesemail = "";
+if (isset($_SESSION["sesemail"])):
+  $sesemail = $_SESSION["sesemail"];
+endif;
+
+$sespesan = "";
+if (isset($_SESSION["sespesan"])):
+  $sespesan = $_SESSION["sespesan"];
+endif;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Judul Halaman</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <h1>Ini Header</h1>
+    <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+      &#9776;
+    </button>
+    <nav>
+      <ul>
+        <li><a href="#home">Beranda</a></li>
+        <li><a href="#about">Tentang</a></li>
+        <li><a href="#contact">Kontak</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section id="home">
+      <h2>Selamat Datang</h2>
+      <?php
+      echo "halo dunia!<br>";
+      echo "nama saya hadi";
+      ?>
+      <p>Ini contoh paragraf HTML.</p>
+    </section>
+<section id="about">
+            <?php
+            $Nama = "M KAESA DUDE LAUDIANIS KOTO &#128526";
+            $NIM = "2511500066";
+            $tempat_lahir = "SUNGAILIAT";
+            $tanggal_lahir = "25 September 2006";
+            $hobi = "Memancinng";
+            $pasangan = "-";
+            $pekerjaan = "MAHASISWA";
+            $orang_tua = "-";
+            $kakak = "-";
+            $adik = "-";
+            ?>
+      <h2>Tentang Saya</h2>
+      <p><strong>NIM:</strong>
+        <?php
+        echo $NIM;
+        ?>
+      </p>
+      <p><strong>Nama Lengkap:</strong>
+        <?php
+        echo $Nama;
+        ?> &#128526;
+        <section id="about">
+            <h2>Tentang kami</h2>
+            <p> <strong>Nama:</strong>M KAESA DUDE LAUDIANIS KOTO</p>
+            <p> <strong>NIM:</strong>2511500066</p>
+            <p> <strong>Tempat Lahir:</strong>SUNGAILIAT</p>
+            <p> <strong>Tanggal Lahir:</strong>25 SEPTEMBER 2006</p>
+            <p> <strong>Hobi:</strong>MEMANCING</p>
+            <p> <strong>Pasangan:</strong>SEDANG DI USAHAKAN</p>
+            <p> <strong>Pekerjaan:</strong>MAHASISWA</p>
+            <p> <strong>Nama Ayah:</strong>-</p>
+            <p> <strong>Nama Ibu:</strong>-</p>
+            <p> <strong>Nama Kakak:</strong>-</p>
+            <p> <strong>Nama Adek:</strong>-</p>
+        </section>
+
+    <section id="contact">
+      <h2>Kontak Kami</h2>
+      <form action="proses.php" method="POST">
+
+        <label for="txtNama"><span>Nama:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+        </label>
+
+        <label for="txtEmail"><span>Email:</span>
+          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
+        </label>
+
+        <label for="txtPesan"><span>Pesan Anda:</span>
+          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+          <small id="charCount">0/200 karakter</small>
+        </label>
+
+
+        <button type="submit">Kirim</button>
+        <button type="reset">Batal</button>
+      </form>
+
+      <?php if (!empty($sesnama)): ?>
+        <br><hr>
+        <h2>Yang menghubungi kami</h2>
+        <p><strong>Nama :</strong> <?php echo $sesnama ?></p>
+        <p><strong>Email :</strong> <?php echo $sesemail ?></p>
+        <p><strong>Pesan :</strong> <?php echo $sespesan ?></p>
+      <?php endif; ?>
+
+
+
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2025 Yohanes Setiawan Japriadi [0344300002]</p>
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
